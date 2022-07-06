d8e4d601a4e86534efe99d3a89c4c380cc86b8d3
Adib Fairuz Ikbar <fairuzadib@gmail.com>
2022-07-07 01:40:52 +0700

    fix profile size

--- apps/user/src/containers/Profile/index.js
+++ apps/user/src/containers/Profile/index.js
@@ -4,7 +4,7 @@ const Profile = () => {
   return (
     <div class="px-6 pt-6">
         <div class="flex items-center mb-5">
-            <div class="flex flex-col justify-center min-w-[175px]">
+            <div class="flex flex-col justify-center mr-8">
                 <div class="w-20 h-20 mb-1 relative">
                     <img class="rounded-full border border-gray-300 shadow-sm" src="https://firebasestorage.googleapis.com/v0/b/instagram-clone-f0931.appspot.com/o/user_placeholder.png?alt=media" alt="user image" />
                 </div>
@@ -12,18 +12,18 @@ const Profile = () => {
                     Adib Fairuz
                 </div>
             </div>
-            <div class="flex flex-1 justify-between items-center">
-                <div class="flex flex-col items-center">
+            <div class="flex flex-1 justify-around items-center">
+                <div class="flex flex-col items-center mr-1">
                     <div class="font-semibold text-lg">14</div>
-                    <div class="font-normal text-lg">Posts</div>
+                    <div class="font-normal text-base">Posts</div>
                 </div>
-                <div class="flex flex-col items-center">
+                <div class="flex flex-col items-center mr-1">
                     <div class="font-semibold text-lg">604</div>
-                    <div class="font-normal text-lg">Followers</div>
+                    <div class="font-normal text-base">Followers</div>
                 </div>
-                <div class="flex flex-col items-center">
+                <div class="flex flex-col items-center mr-1">
                     <div class="font-semibold text-lg">327</div>
-                    <div class="font-normal text-lg">Following</div>
+                    <div class="font-normal text-base">Following</div>
                 </div>
             </div>
         </div>
