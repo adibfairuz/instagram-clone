b71f8529b8e475a1ff175f94214ab5549dace28e
Adib Fairuz Ikbar <fairuzadib@gmail.com>
2022-07-09 03:06:20 +0700

    add mjs to explore webpack rules

--- apps/explore/webpack.config.js
+++ apps/explore/webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
   module: {
 		rules: [
       {
-        test: /\.js$/,
+        test: /\.m?js$/,
         loader: "babel-loader",
         exclude: /node_modules/,
         options: {
